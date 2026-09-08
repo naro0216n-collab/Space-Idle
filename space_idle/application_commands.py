e494b31a29a99bd425da370d9ffea97928eb0c1c
naro0216n-collab <naro0216n@gmail.com>
2026-09-09 08:08:02 +0900

    Route application commands through lane API

--- space_idle/application_commands.py
+++ space_idle/application_commands.py
@@ -13,13 +13,13 @@ from .app_contracts.progression import (
     PauseSurvey, ResumeSurvey, SetSurveyAllocation,
 )
 from .app_contracts.transport import (
-    DispatchVehicle, RefuelVehicle, ProduceVehicle, SubmitCargo, CreateLogisticsRule,
-    PauseLogisticsRule, ResumeLogisticsRule, DeleteLogisticsRule,
+    DispatchVehicle, RefuelVehicle, ProduceVehicle, SubmitCargo, CreateLogisticsLane,
+    PauseLogisticsLane, ResumeLogisticsLane, DeleteLogisticsLane,
 )
 from .app_contracts.contracts import AcceptContract, DispatchContractCargo, DeclineContract
 from .app_contracts.queries import (
     GetCatalog, GetWorld, GetLocation, GetFlowReport, GetBottlenecks, GetProjects, GetBuildOptions, GetLogistics,
-    GetLogisticsSummary, GetRoutes, GetVehicles, GetCargoOrders, GetLogisticsRules, GetTransportMissions,
+    GetLogisticsSummary, GetRoutes, GetVehicles, GetCargoOrders, GetLogisticsLanes, GetTransportMissions,
     GetTransportPlans, GetResearch, GetSurveys, GetContracts,
 )
 
@@ -31,9 +31,9 @@ Command: TypeAlias = (
     StartResearch | PauseResearch | ResumeResearch |
     FundResearchPrototype | SetResearchDemonstrationSite | StartSurvey | PauseSurvey |
     ResumeSurvey | SetSurveyAllocation | DispatchVehicle | RefuelVehicle | ProduceVehicle |
-    SubmitCargo | CreateLogisticsRule | PauseLogisticsRule | ResumeLogisticsRule |
-    DeleteLogisticsRule | AcceptContract | DispatchContractCargo | DeclineContract | AdvanceTime
+    SubmitCargo | CreateLogisticsLane | PauseLogisticsLane | ResumeLogisticsLane |
+    DeleteLogisticsLane | AcceptContract | DispatchContractCargo | DeclineContract | AdvanceTime
 )
-Query: TypeAlias = GetCatalog | GetWorld | GetLocation | GetFlowReport | GetBottlenecks | GetProjects | GetBuildOptions | GetLogistics | GetLogisticsSummary | GetRoutes | GetVehicles | GetCargoOrders | GetLogisticsRules | GetTransportMissions | GetTransportPlans | GetResearch | GetSurveys | GetContracts
+Query: TypeAlias = GetCatalog | GetWorld | GetLocation | GetFlowReport | GetBottlenecks | GetProjects | GetBuildOptions | GetLogistics | GetLogisticsSummary | GetRoutes | GetVehicles | GetCargoOrders | GetLogisticsLanes | GetTransportMissions | GetTransportPlans | GetResearch | GetSurveys | GetContracts
 
 __all__ = [name for name in globals() if not name.startswith('_') and name not in {'TypeAlias'}]
