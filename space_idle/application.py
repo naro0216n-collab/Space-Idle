b201c1e8d61a2a568bcfafdec08d70d26c443e83
naro0216n-collab <naro0216n@gmail.com>
2026-09-09 08:19:56 +0900

    Expose lane logistics through application boundary

--- space_idle/application.py
+++ space_idle/application.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
 from .application_commands import (
-    AcceptContract, AdvanceTime, ApplicationError, CancelBuild, CreateLogisticsRule,
-    DeclineContract, DeleteLogisticsRule, DispatchContractCargo, DispatchVehicle,
-    FundResearchPrototype, GetBuildOptions, GetCatalog, GetContracts, GetLocation, GetFlowReport, GetBottlenecks,
-    GetLogistics, GetLogisticsSummary, GetRoutes, GetVehicles, GetCargoOrders, GetLogisticsRules, GetTransportMissions,
+    AcceptContract, AdvanceTime, ApplicationError, CancelBuild, CreateLogisticsLane,
+    DeclineContract, DeleteLogisticsLane, DispatchContractCargo, DispatchVehicle,
+    FundResearchPrototype, GetBuildOptions, GetCatalog, GetContracts, GetLocation,
+    GetFlowReport, GetBottlenecks, GetLogistics, GetLogisticsSummary, GetRoutes,
+    GetVehicles, GetCargoOrders, GetLogisticsLanes, GetTransportMissions,
     GetProjects, GetResearch, GetSurveys, GetTransportPlans, GetWorld,
-    PauseBuild, PauseFacility, PauseLogisticsRule, PauseResearch, PauseSurvey,
-    PlanBuild, PlanFacilityUpgrade, ProduceVehicle, RefuelVehicle, ResumeBuild, ResumeFacility,
-    ResumeLogisticsRule, ResumeResearch, ResumeSurvey, SetConstructionWeight,
-    SetFacilityProcess, SetPowerPriority, SetProjectImportSource,
-    SetProjectImportTransport, SetProjectLocalFraction, SetProjectLocalMaterial,
-    SetProjectPriority, SetProjectSourcingPolicy,
+    PauseBuild, PauseFacility, PauseLogisticsLane, PauseResearch, PauseSurvey,
+    PlanBuild, PlanFacilityUpgrade, ProduceVehicle, RefuelVehicle, ResumeBuild,
+    ResumeFacility, ResumeLogisticsLane, ResumeResearch, ResumeSurvey,
+    SetConstructionWeight, SetFacilityProcess, SetPowerPriority,
+    SetProjectImportSource, SetProjectLocalFraction, SetProjectLocalMaterial,
+    SetProjectPriority, SetProjectSourcingPolicy, SetResearchPrototypeSite,
     SetResearchDemonstrationSite, SetSurveyAllocation, StartResearch, StartSurvey,
     SubmitCargo,
 )
@@ -30,5 +31,7 @@ class GameApplication(ApplicationCommandMixin, ApplicationQueryMixin):
     def content_id(self) -> str:
         return self._simulation.content_id
 
-    def advance_offline(self, elapsed_real_seconds: float, policy: OfflineProgressPolicy) -> OfflineProgressResult:
+    def advance_offline(
+        self, elapsed_real_seconds: float, policy: OfflineProgressPolicy
+    ) -> OfflineProgressResult:
         return self._simulation.advance_offline(elapsed_real_seconds, policy)
