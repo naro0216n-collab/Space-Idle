4e4bbd2cb4c0022fd5120f563f4ec544934c4363
naro0216n-collab <naro0216n@gmail.com>
2026-09-09 01:01:14 +0900

    API: export time-controlled server composition

--- space_idle/api/__init__.py
+++ space_idle/api/__init__.py
@@ -1,5 +1,6 @@
 from .codec import ApiPayloadError, command_schema, decode_command, to_jsonable
-from .http_server import ApiServerConfig, SpaceIdleHTTPServer, create_server
+from .http_server import ApiServerConfig, SpaceIdleHTTPServer
+from .time_http_server import create_server
 from .runtime import GameRuntime, RevisionConflict, RuntimeResult
 
 __all__ = [
